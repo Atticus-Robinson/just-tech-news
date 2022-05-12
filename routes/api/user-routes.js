3451b84721eb05cd955a6a23e2df588b7a61e2a5
Atticus-Robinson <acerobinson2@gmail.com>
2022-05-12 09:49:17 -0500

    finished routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const { json } = require("express/lib/response");
 const { User } = require("../../models");
 
 //GET /api/users
@@ -13,15 +14,75 @@ router.get("/", (req, res) => {
 });
 
 //GET /api/users/1
-router.get("/:id", (req, res) => {});
+router.get("/:id", (req, res) => {
+  User.findOne({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(404).json({ message: "No user found with this id" });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 //POST /api/users
-router.post("/", (req, res) => {});
+router.post("/", (req, res) => {
+  User.create({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+  })
+    .then((dbUserData) => res.json(dbUserData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 //PUT /api/users/1
-router.put("/:id", (req, res) => {});
+router.put("/:id", (req, res) => {
+  User.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbUserData) => {
+      if (!dbUserData[0]) {
+        res.status(404).json({ message: "No user found with this id" });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 //DELETE /api/users/1
-router.delete("/:id", (req, res) => {});
-
-module.exports = router;
+router.delete("/:id", (req, res) => {
+  User.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(404).json({ message: "No user found with this id" });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
