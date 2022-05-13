5488b1522f6bcfb24a3c8fe347def7b87a777402
Atticus-Robinson <acerobinson2@gmail.com>
2022-05-12 22:15:35 -0500

    user login start

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -33,6 +33,7 @@ router.get('/:id', (req, res) => {
     });
 });
 
+//Create new user
 router.post('/', (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
   User.create({
@@ -46,6 +47,27 @@ router.post('/', (req, res) => {
       res.status(500).json(err);
     });
 });
+
+//Login route
+router.post('/login', (req, res) => {
+  //Expects {email: 'acerobinson2@gmail.com', password: 'password1234'}
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+  }).then(dbUserData => {
+    if (!dbUserData) {
+      res.status(400).json({ message: 'No user with that email address!'});
+      return;
+    }
+
+    res.json({ user: dbUserData });
+  })
+
+  //Verify user
+  // Query operation
+
+})
 
 router.put('/:id', (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
